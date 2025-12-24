d3d27c13bd832c9217debe19c219646f6a1878f3
SriniAccenture96 <srinivas.r.nagapuri@accenture.com>
2025-12-24 13:33:57 +0000

    Refactor settings and URLs to support codespace environment and improve API root response

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure-ikse_m10hi$v!^(ere&f(klf!su*#gs$ie^7+8so66%e4k4!!z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+
+# Allow localhost and codespace URL
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
